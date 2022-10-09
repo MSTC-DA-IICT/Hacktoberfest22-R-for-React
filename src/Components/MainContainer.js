2debebd9632287b4afd1f3b9f8ecd3dbaf27bdb3
Jam-Burger <jay40793@gmail.com>
2022-10-10 00:47:07 +0530

    create product card

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
-function MainContainer() {
+import ProductCard from './ProductCard'
+function MainContainer(props) {
   return (
-    <div>MainContainer</div>
+    <div>
+      <div className='md:flex-wrap'>Main container</div>
+      <div className='md:flex md:flex-wrap'>
+        {props.data.map((product, i) => (
+          <ProductCard key={i} data={product} />
+        ))}
+      </div>
+    </div>
   )
 }
 
